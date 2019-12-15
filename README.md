b5fe478cd59609b45f230af52900f7a2ffbbbbb4
Ashwani Goswami <goswami17829@gmail.com>
2019-12-16 01:02:34 +0530

    Done the basics functionality and added the readme(How to play)

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # color-pattern-game
+
+This is a pattern game. It examine your memory power ability, build in pure Vanilla Javascript.
+
+## How To Play
+
+* There are four boxes(red, green, yellow and blue).
+* Hit the play button, and see carefully the pattern.
+* You need to choose the box in order of pattern.
+* Every correct attempt, will get you on the next level and Wrong attempt will get back on the first level again.
+* You need to cross all the 10 levels to win the game.
+
+# Best of luck
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var playBtn = document.querySelector('.play-btn');
+var message = document.querySelector('.message');
 var boxes = document.querySelectorAll('.box');
-var level = 2;
+var levelElem = document.querySelector('.level');
+var mainContainer = document.querySelector('.main-container');
+var level = 1;
 var colors = ["red", "blue", "yellow", "green"];
 var pattern = [];
+var selectedBox = [];
+var isPlayed = false;
 
 function generatePattern(arr=[]) {
     for(var i=0; arr.length+1 <= level; i++) {
@@ -16,13 +21,21 @@ function generatePattern(arr=[]) {
 
 generatePattern(pattern);
 
-// function changeColor(item) {
-//     return item.style.opacity = '0.3';
-// }
+function levelIncDec(status) {
+    if(!status) {
+        level = level;
+        levelElem.innerHTML = `Level: ${level}`
+    } else {
+        level = level + 1;
+        levelElem.innerHTML = `Level: ${level}`
+    }
+}
+
 
 function highlight(elem){
+    console.log(elem)
     var orig = elem.style.opacity;
-    elem.style.opacity = '0.3';
+    elem.style.opacity = '1';
     setTimeout(function(){
          elem.style.opacity = orig;
     }, 500);
@@ -30,18 +43,51 @@ function highlight(elem){
 
 
 function showPattern() {
-    var targetBoxex = [];
-    boxes.forEach(item => {
-        if(pattern.indexOf(item.id) !== -1) {
-            // setInterval(highlight(item), 500)
-            // highlight(item)
-            targetBoxex.push(item.id);
-        }
+    pattern.forEach((item, i) => {
+        setTimeout(highlight, 500*i, document.getElementById(item))
     })
-    targetBoxex.forEach(item => {
-        setTimeout(highlight, 500, document.getElementById(item))
-    })
-    generatePattern(pattern )
+
+    // generatePattern(pattern );
+    isPlayed = true;
+    playBtn.style.display = "none";
+    message.innerHTML = 'Choose the boxes in order of pattern';
 }
 
-playBtn.addEventListener('click', showPattern)
+function compareResult() {
+    if(selectedBox.length === pattern.length) {
+        message.innerHTML = `Well Done! Level ${level + 1}`;
+        isPlayed = false;
+        pattern = [];
+        selectedBox = [];
+        levelIncDec(true);
+        playBtn.style.display = 'block';
+        boxes.forEach(box => {
+            box.style.border = 'none';
+        })
+        generatePattern(pattern)
+    }
+    if(level === 10) {
+        mainContainer.style.display = 'none';
+        document.querySelector('.result').innerHTML = 'Yeah! You won.'
+        
+    }
+}
+
+boxes.forEach(box => {
+    box.addEventListener('click', (e) => {
+        if(!isPlayed) return;
+        selectedBox.push(e.target.id);
+        box.style.border = '2px solid black';
+        console.log(selectedBox, pattern)
+        for(var i=0; i< selectedBox.length; i++) {
+            if(selectedBox[i] !== pattern[i]) {
+                message.innerHTML = 'Wrong Attempt! Try Again.';
+                levelIncDec(false)
+                setTimeout(function(){window.location.reload()}, 5000)
+            }
+        }
+        compareResult()
+    })
+})
+
+playBtn.addEventListener('click', showPattern);
